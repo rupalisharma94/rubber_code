502888d931ffd48d2f746d2e1a8ef3b7804665e5
rupalisharma94 <rups145@gmail.com>
2021-11-15 16:08:40 +0530

    rubber.htm

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; N.R.Rubber Industry | ADMIN LOGIN |<a href="https://nrrubberindustry.com/" target="_blank"  > Designed by : Sikharthy Infotech Pvt Ltd</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ admin/includes/header.php
@@ -0,0 +1,31 @@
+<div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand">
+
+                    <img src="assets/img/logo-2.png" />
+                </a>
+
+            </div>
+
+            <div class="right-div">
+                <a href="logout.php" class="btn btn-danger pull-right">LOG ME OUT</a>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
